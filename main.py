97f9713a487e45c5de9966af6a110cccd2ff14b8
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2020-07-21 22:18:37 +1000

    Add random weights to Sin layer

--- main.py
+++ main.py
@@ -7,10 +7,38 @@ import torch
 data = torch.randn(1000, 2)
 #data = (torch.randn(1000, 2) + 0.5).sin()
 
+# x = data[:, 0].round()
+# y = data[:, 1]
 
-class Sin(torch.nn.Module):
-    def __init__(self, out_features):
-        super(Sin, self).__init__()
+# div = (data[:, 0].square() + data[:, 1].square()).sqrt()
+# x = data[:, 0] / div
+# y = data[:, 1] / div
+
+# x = data[:, 0]
+# y = (data[:, 0] * 3.0).sin()
+
+x, y = [], []
+for x_, y_ in data:
+
+    if x_ < 0.0:
+        x_ = x_ + 1.0
+        div = (x_ ** 2.0 + y_ ** 2.0) ** 0.5
+        x_ = x_ / div * 0.50 - 0.75
+        y_ = y_ / div * 1.25 + 0.50
+    else:
+        x_ = x_ - 1.0
+        div = (x_ ** 2.0 + y_ ** 2.0) ** 0.5
+        x_ = x_ / div * 0.75 + 0.75
+        y_ = y_ / div * 0.75 - 0.75
+
+    x.append(x_)
+    y.append(y_)
+x, y = torch.tensor(x), torch.tensor(y)
+
+
+class ResSin(torch.nn.Module):
+    def __init__(self, out_features, bendy_bit_lr=0.5):
+        super(ResSin, self).__init__()
 
         self.bendiness = torch.nn.Parameter(torch.zeros(out_features))
 
@@ -22,16 +50,27 @@ class Sin(torch.nn.Module):
             torch.zeros(out_features),
         )
 
-        self.tau = torch.nn.Parameter(
-            torch.acos(torch.tensor(0.0)) * 4.0,
-            requires_grad=False,
+        pi = (torch.acos(torch.tensor(0.0)) * 2.0).item()
+        self.fix_weight = torch.nn.Parameter(
+            torch.ones(out_features) * pi
+        )
+        self.var_weight = torch.nn.Parameter(
+            torch.randn(out_features) * pi * 2
         )
 
         self.out_features = out_features
+        self.bendy_bit_lr = bendy_bit_lr
 
     def forward(self, x):
-        bias = self.fix_bias + self.var_bias
-        return x + self.bendiness * (x * self.tau + bias).sin()
+        weight = \
+            self.fix_weight + \
+            self.var_weight
+        bias = \
+            self.fix_bias + \
+            self.var_bias
+        return \
+            self.bendiness * self.bendy_bit_lr * (x * weight + bias).sin() + \
+            x
 
 
 class ResLinear(torch.nn.Module):
@@ -68,17 +107,20 @@ class ResLinear(torch.nn.Module):
 
 layers = []
 
-startRl = ResLinear(2, 33)
-startHi = Sin(33)
+startRl = ResLinear(2, 99)
+startHi = ResSin(99)
 layers.append(startRl)
 layers.append(startHi)
 
-midRl = ResLinear(33, 33)
-midHi = Sin(33)
-layers.append(midRl)
-layers.append(midHi)
+for _ in range(40):
+    layers.append(ResLinear(99, 99))
+    layers.append(ResSin(99))
+# midRl = ResLinear(99, 99)
+# midHi = ResSin(99)
+# layers.append(midRl)
+# layers.append(midHi)
 
-stopRl = ResLinear(33, 2)
+stopRl = ResLinear(99, 2)
 layers.append(stopRl)
 
 parameters = sum(
@@ -87,36 +129,20 @@ parameters = sum(
 )
 optimizer = torch.optim.SGD(
     parameters,
-    lr=1e-3,
+    lr=1e-2,
     momentum=0.9,
-    weight_decay=1e-2,
+    weight_decay=1e-4,
 )
 
 for i in itertools.count():
     out = data
 
-    out = startRl(out)
-    out = startHi(out)
+    for l in layers:
+        out = l(out)
 
-    for _ in range(1000):
-        out = midRl(out)
-        out = midHi(out)
-
-    out = stopRl(out)
-
-
-    # loss = \
-    #     (out[:, 0] - data[:, 0].round()).square().mean() + \
-    #     (out[:, 1] - data[:, 1]).square().mean()
-
-    div = (data[:, 0].square() + data[:, 1].square()).sqrt()
-    x = data[:, 0] / div
-    y = data[:, 1] / div
-    loss = (out[:, 0] - x).square().mean() + (out[:, 1] - y).square().mean()
-
-    # loss = \
-    #     (out[:, 0] - data[:, 0]).square().mean() + \
-    #     (out[:, 1] - (data[:, 0] * 3.0).sin()).square().mean()
+    loss = \
+        (out[:, 0] - x).square().mean() + \
+        (out[:, 1] - y).square().mean()
 
     optimizer.zero_grad()
     loss.backward()
@@ -128,11 +154,8 @@ for i in itertools.count():
         # print(layers[2]._weight())
 
         plt.axis((-3.0, 3.0, -3.0, 3.0))
-        plt.scatter(
-            out.data[:, 0],
-            out.data[:, 1],
-            s=0.5,
-        )
+        plt.scatter(x, y, s=0.01, color='k', alpha=0.25)
+        plt.scatter(out.data[:, 0], out.data[:, 1], s=0.5, color='b')
         plt.savefig('figures/out-%05d.png' % i)
         plt.close()
 
